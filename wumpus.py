15388cf891ed7e49246e0fd3bd4034e19a6eb996
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-10-18 21:06:54 +0600

    slider added

--- wumpus.py
+++ wumpus.py
@@ -10,20 +10,31 @@ class Mygui:
 		Label(self.master, text="Number of Arrow ", font=5).grid(row=1, column=0)
 		Label(self.master, text="Number of Pit ", font=5).grid(row=2, column=0)
 
-		self.w = Entry(self.master, width= 15, borderwidth=3)
+		# self.w = Entry(self.master, width= 15, borderwidth=3)
+		
+		self.w = Scale(self.master, from_=0, to=10, length=150, orient=HORIZONTAL, sliderrelief='ridge', highlightthickness=1, activebackground='gray')
 		self.w.grid(row=0, column=1)
+		self.w.set(3)
 
-		self.a = Entry(self.master, width= 15, borderwidth=3)
-		self.a.grid(row=1, column=1)
+		# self.a = Entry(self.master, width= 15, borderwidth=3)
+		# self.a.grid(row=1, column=1)
+		self.a = Scale(self.master, from_=0, to=10, length=150, orient=HORIZONTAL,sliderrelief='ridge', highlightthickness=1, activebackground='gray')
+		self.a.grid(row=1, column=1) #troughcolor='green',
+		self.a.set(3)
 
-		self.p = Entry(self.master, width= 15, borderwidth=3)
+		# self.p = Entry(self.master, width= 15, borderwidth=3)
+		# self.p.grid(row=2, column=1)
+		self.p = Scale(self.master, from_=0, to=10, length=150, orient=HORIZONTAL, sliderrelief='ridge', highlightthickness=1, activebackground='gray')
 		self.p.grid(row=2, column=1)
+		self.p.set(3)
 
-		submit = Button(self.master, text="start", command=self.click, font=("Comic Sans Ms", 12), fg="red",
+		self.submit = Button(self.master, text="start", command=self.click, font=("Comic Sans Ms", 12), fg="red",
 		relief="solid", borderwidth=1, padx=10, pady=3, highlightthickness=2, highlightcolor="black")
 		#img = PhotoImage(file="D:/Sadi/tkinter/start.jpg")
 		#submit.config(image=img)
-		submit.grid(row=3, column=0, columnspan=2)
+		self.submit.grid(row=3, column=0, columnspan=2)
+		self.submit.bind("<Enter>", self.on_enter)
+		self.submit.bind("<Enter>", self.on_leave)
 
 
 	def click(self):
@@ -47,6 +58,12 @@ class Mygui:
 		
 
 		#print("done")
+
+	def on_enter(self, e):
+		self.submit['background'] = 'blue'
+
+	def on_leave(self, e):
+		self.submit['background'] = 'SystemButtonFace'
 
 root = Tk()
 root.title("Wumpus")
